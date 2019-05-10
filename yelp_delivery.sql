e01c176df716504c9f4e0ea72c4edacbb8b7708c
Geenort <tronguyen92@gmail.com>
2019-05-09 21:57:32 -0700

    finished v2

--- yelp_delivery.sql
+++ yelp_delivery.sql
@@ -1,21 +1,25 @@
-CREATE DATABASE yelp_delivery_db;
+DROP DATABASE IF EXISTS yelp_delivery_db;
+
+CREATE DATABASE yelp_delivery_db 
+	CHARACTER SET utf8mb4 
+    COLLATE utf8mb4_unicode_ci;
 
 USE yelp_delivery_db;
 
-CREATE TABLE businesses(
+CREATE TABLE businesses (
 business_id varchar(50),
 name1 varchar(50),
 address varchar(50),
 state varchar(50),
 zip_code varchar(50),
 has_catering varchar(50),
-stars float,
+stars float,businesses
 review_count int,
 primary key (business_id),
 foreign key (zip_code) references median_income(zip_code)
 );
 
-CREATE TABLE median_income(
+CREATE TABLE median_income (
 zip_code varchar(50),
 median_income int,
 primary key (zip_code)
